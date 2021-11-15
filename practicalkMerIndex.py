a696e2e0116c187b45df215f246757c762733c96
ldups <38325402+ldups@users.noreply.github.com>
2021-11-15 16:41:16 -0500

    Finished lesson

--- practicalkMerIndex.py
+++ practicalkMerIndex.py
@@ -1,10 +1,11 @@
 import bisect
+import sys
 class Index(object):
-    def ___init___(self, t, k):
+    def __init__(self, t, k):
         self.k = k
         self.index = []
         for i in range(len(t) - k + 1):
-            self.index.append(t[i, i+k], i)
+            self.index.append((t[i:i+k], i))
         self.index.sort()
     
     def query(self, p):
@@ -13,3 +14,25 @@ class Index(object):
         hits = []
         while i < len(self.index):
             if self.index[i][0] != kmer:
+                break
+            hits.append(self.index[i][1])
+            i += 1
+        return hits
+
+    def queryIndex(index, p, t):
+        k = index.k
+        offsets = []
+        for i in index.query(p):
+            #verification
+            if p[k:] == t[i+k:i+len(p)]:
+                offsets.append(i)
+        return offsets
+
+t = 'GCTACGATCTAGAATCTA'
+p = 'TCTA'
+
+index = Index(t, 2)
+print(index.queryIndex(p, t))
+
+
+
