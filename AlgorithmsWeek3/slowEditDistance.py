7f447fb3b7f996ff8a098f34b848fcfb2fae0b1a
ldups <38325402+ldups@users.noreply.github.com>
2021-11-21 18:44:31 -0500

    Week 3- dynamic programming

---
+++ AlgorithmsWeek3/slowEditDistance.py
@@ -0,0 +1,9 @@
+def edDistRecursive(a, b):
+    if len(a) == 0:
+        return len(b)
+    if len(b) == 0:
+        return len(a)
+    delt = 1 if a[-1] != b[-1] else 0
+    return min(edDistRecursive(a[-1], b[-1]) + delt, edDistRecursive(a[-1], b) + 1, edDistRecursive(a, b[-1]) + 1)
+
+print(edDistRecursive('Hello', 'helloj'))
